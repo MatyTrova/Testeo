26ddea228759320ed974384dc3476892b456160c
Matias Trovatto <118058206+MatyTrova@users.noreply.github.com>
2023-07-26 11:44:23 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,20 +7,6 @@ st.set_page_config(
     initial_sidebar_state="expanded"
     )
 
-# Título de la página
-st.title("**Dashboard Experiencias**")
-
-st.markdown("---")
-# Imagen
-#st.image("", use_column_width=True)
-# Línea divisoria
-st.markdown("---")
-
-# Encabezado
-st.header("¡Bienvenido, ... !")
-
-# Texto descriptivo
-st.write("Te damos la bienvenida a ....")
 import streamlit as st
 
 # Contraseña válida (reemplaza con tu contraseña)
@@ -30,47 +16,68 @@ contraseña_valida = "mi_contraseña_secreta"
 def verificar_contraseña(contraseña_ingresada):
     return contraseña_ingresada == contraseña_valida
 
-# Página de inicio
-def pagina_inicio():
-    st.title("Página de Inicio")
-    st.write("Bienvenido a la página de inicio. Por favor, ingrese su contraseña.")
+# Función para mostrar el contenido de la página Feedback
+def mostrar_feedback():
+    st.title("Página de Feedback")
+    # Aquí puedes agregar el contenido específico de la página Feedback
 
-    # Obtener contraseña ingresada por el usuario
-    cliente_id = st.text_input('Ingrese su ID de cliente:')
+# Función para mostrar el contenido de la página Recompra
+def mostrar_recompra():
+    st.title("Página de Recompra")
+    # Aquí puedes agregar el contenido específico de la página Recompra
 
-    # Verificar la contraseña
-    if cliente_id and verificar_contraseña(cliente_id):
-        st.success("Contraseña válida. Acceso concedido.")
-        # Establecer el estado de autenticación de la sesión
-        st.session_state.autenticado = True
-    else:
-        st.error('ID de cliente no válido. Intente nuevamente con un ID válido.')
+# Función principal
+def main():
+    st.set_page_config(page_title="Mi Aplicación Streamlit")
 
-# Página protegida que solo se mostrará si el usuario está autenticado
-def pagina_protegida():
-    # Verificar si el usuario está autenticado
-    if not st.session_state.get('autenticado'):
-        st.error("Debe ingresar una contraseña válida en la página de inicio para acceder a esta página.")
-        st.stop()
+    # Contraseña válida (reemplaza con tu contraseña)
+    contraseña_valida = "mi_contraseña_secreta"
 
-    st.title("Página Protegida")
-    st.write("Esta es una página protegida que solo se muestra si el usuario ha ingresado la contraseña válida en la página de inicio.")
+    # Función para verificar la contraseña ingresada
+    def verificar_contraseña(contraseña_ingresada):
+        return contraseña_ingresada == contraseña_valida
 
-# Mostrar la página correspondiente según la ruta de la URL
-pagina = st.sidebar.selectbox("Seleccione una página", ["Inicio", "Página Protegida"])
+    # Página de inicio
+    def pagina_inicio():
+        st.title("Página de Inicio")
+        st.write("Bienvenido a la página de inicio. Por favor, ingrese su contraseña.")
 
-if pagina == "Inicio":
-    pagina_inicio()
-elif pagina == "Página Protegida":
-    pagina_protegida()
+        # Obtener contraseña ingresada por el usuario
+        cliente_id = st.text_input('Ingrese su ID de cliente:')
 
-# Sección de información
-st.subheader("¿Qué puedes encontrar aquí?")
-st.write("En esta aplicación, descubrirás 4 secciones:")
-st.write("+ **Feedback**: ...")
-st.write("+ **Recompra**: ...")
-st.write("+ **Vacunas**: ...")
-st.write("+ **Cirugia**: ...")
+        # Verificar la contraseña
+        if cliente_id and verificar_contraseña(cliente_id):
+            st.success("Contraseña válida. Acceso concedido.")
+            # Establecer el estado de autenticación de la sesión
+            st.session_state.autenticado = True
+        else:
+            st.error('ID de cliente no válido. Intente nuevamente con un ID válido.')
 
-# Línea divisoria
-st.markdown("---")
+    # Página protegida que solo se mostrará si el usuario está autenticado
+    def pagina_protegida():
+        # Verificar si el usuario está autenticado
+        if not st.session_state.get('autenticado'):
+            st.error("Debe ingresar una contraseña válida en la página de inicio para acceder a esta página.")
+            st.stop()
+
+        st.title("Página Protegida")
+        st.write("Esta es una página protegida que solo se muestra si el usuario ha ingresado la contraseña válida en la página de inicio.")
+
+    # Opciones del sidebar para seleccionar página
+    opciones_paginas = ["Inicio", "Feedback", "Recompra"]
+    pagina_seleccionada = st.sidebar.selectbox("Selecciona una página:", opciones_paginas)
+
+    # Mostrar el contenido de la página seleccionada
+    if pagina_seleccionada == "Inicio":
+        pagina_inicio()
+    elif pagina_seleccionada == "Feedback":
+        pagina_protegida()
+        mostrar_feedback()
+    elif pagina_seleccionada == "Recompra":
+        pagina_protegida()
+        mostrar_recompra()
+
+# Iniciar la aplicación
+if __name__ == "__main__":
+    main()
+
