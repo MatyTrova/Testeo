e4f1f8778f9a4e7be0e12ad2ab983e393d352f52
Matias Trovatto <118058206+MatyTrova@users.noreply.github.com>
2023-07-26 14:11:49 -0300

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -159,7 +159,8 @@ def main():
         reviews_por_dia = df_filtered[['fecha',"msgBody"]].value_counts().reset_index()
         reviews_por_dia1 = reviews_por_dia[reviews_por_dia["msgBody"].str.contains("Positivo")]
         reviews_por_dia2 = reviews_por_dia[reviews_por_dia["msgBody"].str.contains("Negativo")]
-        fig, ax = plt.subplots(figsize=(4, 3))  
+        fig, ax = plt.subplots()
+        fig.set_size_inches(6, 4)  
         sns.set(style="whitegrid")
         ax = sns.lineplot(x="fecha", y="count", marker='o', color='green',data=reviews_por_dia1, label="Positivo",linewidth=4)
         plt.plot(reviews_por_dia2['fecha'], reviews_por_dia2['count'], marker='o', color='blue', label='Negativo',linewidth=4)
@@ -295,7 +296,8 @@ def main():
         df_recompra['fecha'] = pd.to_datetime(df_recompra['fecha'])
         registros_por_dia = df_recompra['fecha'].value_counts().reset_index()
         registros_por_dia.columns = ['fecha', 'cantidad']
-        fig, ax = plt.subplots(figsize=(4, 3)) 
+        fig, ax = plt.subplots()
+        fig.set_size_inches(6, 4) 
         sns.set(style="whitegrid")
         ax = sns.lineplot(x="fecha", y="cantidad", marker='o', color='b',data=registros_por_dia,linewidth=4)
         plt.xlabel('')
